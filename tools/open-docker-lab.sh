7ee0526f3117efb0acd3e9ddc0cf82b10bb26ad8
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-07 02:12:46 +0800

    tools: Add desktop shortcut
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- tools/open-docker-lab.sh
+++ tools/open-docker-lab.sh
@@ -10,4 +10,10 @@ pwd=ubuntu
 
 which $browser 2>&1>/dev/null \
     && ($browser $url 2>&1>/dev/null &) \
-    && echo "Please login $url with password: $pwd" && exit 0
+    && echo "Please login $url with password: $pwd"
+
+
+# Create local shotcut on  Desktop
+echo '#!/usr/bin/env xdg-open' >  ~/Desktop/markdown-lab.desktop
+cat $TOP_DIR/markdown-lab.desktop | sed "s%Exec=.*%Exec=$browser $url%g" | sed "s%lxterminal.xpm%chromium-browser.png%g">> ~/Desktop/markdown-lab.desktop
+chmod a+x ~/Desktop/markdown-lab.desktop
